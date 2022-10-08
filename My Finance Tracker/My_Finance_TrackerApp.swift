e63cd140c863e28de224a15247c7ad5421c87650
Fardi <fardi01.pro@gmail.com>
2022-10-08 09:52:22 +0200

    Initial Commit

---
+++ My Finance Tracker/My_Finance_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  My_Finance_TrackerApp.swift
+//  My Finance Tracker
+//
+//  Created by fardi Issihaka on 08/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct My_Finance_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
